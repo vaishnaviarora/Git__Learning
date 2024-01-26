29951aa07520f43696c9afc344ab18a191f55096
Vaishnavi <2020bit013@sggs.ac.in>
2024-01-26 14:34:18 +0530

    clear

--- example_1.py
+++ example_1.py
@@ -2,11 +2,5 @@ print("Hello World")
 if(7>5):
     print("greater")
 else:
-<<<<<<< HEAD
     print("less")
-=======
-    if(7<5):
-        print("less")
-    else:
-        print("equal")
->>>>>>> second
+
